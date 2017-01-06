e04e03579660c51d770672f38ddacd62b0475bcb
Eri Irawan <email": null>
2017-01-06 16:22:14 +0700

    Board

--- sudoku.js
+++ sudoku.js
@@ -1,12 +1,31 @@
 "use strict"
 
 class Sudoku {
-  constructor(board_string) {}
+  constructor(board_string) {
+    this.arrBoard  = [];
+    this.strSudoku = board_string;
+  }
 
   solve() {}
 
   // Returns a string representing the current state of the board
-  board() {}
+  board() {
+    for(let i = 0; i < this.strSudoku.length; i += 9){
+      let arrTemp = [];
+      for(let j = 0; j < 9; j++){
+        let idx = i+j;
+        arrTemp.push(this.strSudoku[idx]);
+      }
+      this.arrBoard.push(arrTemp);
+      // i += 8;
+    }
+    return this.arrBoard;
+  }
+
+  checkRow(){
+
+  }
+
 }
 
 // The file has newlines at the end of each line,
