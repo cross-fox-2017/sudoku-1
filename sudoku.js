2dbe64e364e1b23900060091517aab0ddcc30aaa
Your Name <name@example.com>
2017-01-08 23:30:56 +0700

    Create Class Sudoku

--- sudoku.js
+++ sudoku.js
@@ -2,3 +2,13 @@
 
 var fs = require('fs')
 var board_string = fs.readFileSync('data.txt').toString().split("\n")[0].trim()
+
+class Sudoku {
+    constructor(board_string) {
+        this._board_string = board_string;
+        this._papan = [];
+        this._tampung = [];
+        this._ulang = 0;
+    }
+
+}
